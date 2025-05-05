88af40ccd0509f23334793f06f8b29fa841fb2f3
fasilofficial <muhammedfasilofficial@gmail.com>
2025-05-05 19:24:02 +0530

    Update timestamp

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Configurations
 define('AUTH_TOKEN', "b20617bc3a21941c43eefae9e4084727");
-define('TIMESTAMP', date("Y-m-d H:i:s")); // Production
+define('TIMESTAMP', date("Y-m-d H:i:s", strtotime("-3 minutes"))); // Production
 // define('TIMESTAMP', "2025-04-01 00:00:00"); // Testing
 define('LEAD_FILE', __DIR__ . '/processed_leads.txt');
 define('DEFAULT_ASSIGNED_USER_ID', '10'); // Wihanga Sathsara
